a0bd8feecb9e22acbae679b4ed5364f476f5feea
mcalvo96 <mcalo96@gmail.com>
2018-12-15 23:36:24 -0600

    cambio 2

--- Sistema.Proyecto/Logica/Accion/UsuarioController.cs
+++ Sistema.Proyecto/Logica/Accion/UsuarioController.cs
@@ -1,39 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Sistema.Proyecto.Logica.Accion
 {
-    public class UsuarioController : ApiController
+    public class UsuarioController : Controller
     {
-        // GET: api/Usuario
-        public IEnumerable<string> Get()
+        // GET: Usuario
+        public ActionResult Index()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Usuario/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Usuario
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Usuario/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Usuario/5
-        public void Delete(int id)
-        {
+            return View();
         }
     }
 }
