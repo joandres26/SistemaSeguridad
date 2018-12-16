4e448cf470d0e17736a6b81953df77c8fcd6e73b
mcalvo96 <mcalo96@gmail.com>
2018-12-15 21:43:29 -0600

    se hizo cambio en HomeController

--- Sistema.UI/Controllers/HomeController.cs
+++ Sistema.UI/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Sistema.UI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Josa es gay.";
 
             return View();
         }
